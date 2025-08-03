85e2f3851ce8e6fb409ecfb0324d22ce6a6a8a7f
sgreenberg-lsr <104652421+sgreenberg-lsr@users.noreply.github.com>
2025-08-03 09:27:45 -0400

    ?? ''

--- src/abstracts/AbstractSchema.php
+++ src/abstracts/AbstractSchema.php
@@ -126,12 +126,12 @@ abstract class AbstractSchema extends Component
             $prefix = Yii::$app->controller->db->tablePrefix ?: null;
         }
 
-        if (strpos($string, $prefix ?? '') === false) {
+        if (strpos($string ?? '', $prefix ?? '') === false) {
             return $string;
         }
 
-        $position = strpos($string, $prefix ?? '') + strlen($prefix);
+        $position = strpos($string ?? '', $prefix ?? '') + strlen($prefix);
 
-        return substr($string, $position);
+        return substr($string ?? '', $position);
     }
 }
